56e881b8c11bd3b76c067e101d6b5e628ef01a6c
David Jones <drj@pobox.com>
2017-12-02 08:34:25 +0000

    Sum all the things

--- 2/2.go
+++ 2/2.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-        "bufio"
+	"bufio"
 	"fmt"
-        "os"
 	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,8 +16,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-        scanner := bufio.NewScanner(inp)
-        for scanner.Scan() {
-          fmt.Println(scanner.Text())
-        }
+	sum := 0.0
+
+	lines := bufio.NewScanner(inp)
+	for lines.Scan() {
+		min := math.MaxFloat64
+		max := -math.MaxFloat64
+
+		line := lines.Text()
+		words := bufio.NewScanner(strings.NewReader(line))
+		words.Split(bufio.ScanWords)
+		for words.Scan() {
+			w := words.Text()
+			f, err := strconv.ParseFloat(w, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			min = math.Min(min, f)
+			max = math.Max(max, f)
+		}
+		sum += max - min
+		fmt.Println(min, max)
+	}
+	fmt.Println(sum)
 }
