8d073d7a8758f09b46fbe105b648d19a87e8a21c
David Jones <drj@pobox.com>
2017-12-03 10:23:14 +0000

    Fix all the bugs

--- 3b/3b.go
+++ 3b/3b.go
@@ -14,6 +14,10 @@ func main() {
 	fmt.Println(x, y)
 
 	fmt.Println(math.Abs(float64(x)) + math.Abs(float64(y)))
+
+	for i := 1; i < 10; i++ {
+		fmt.Println(cartesian(i))
+	}
 }
 
 // Convert from spiral system (starting at 1) to cartesian.
@@ -24,44 +28,48 @@ func cartesian(spiral int) (x, y int) {
 	// until it is a square of cells just big enough
 	// to include the input cell.
 
+	if spiral == 1 {
+		return 0, 0
+	}
+
 	// The input lies on on the outside edge
 	// of this square grid, which we will say is of size n.
 	// n is odd, and the bottom-right cell is n*n.
 	// Find n.
 	n := int(math.Ceil(math.Sqrt(float64(spiral))))
 	n |= 1
+	k := n - 1
 
 	// The 4 corners of the square grid are:
 	//                 side 2
-	//     n*n - 2*n  ~~~~~~~~  n*n - 3*n
+	//     n*n - 2*k  ~~~~~~~~  n*n - 3*k
 	//         '                    '
 	//         '                    '
 	// side 1  '                    '  side 3
 	//         '                    '
 	//         '                    '
-	//     n*n - n  ~~~~~~~~~~~~~  n*n
+	//     n*n - k  ~~~~~~~~~~~~~  n*n
 	//                 side 0
+	// Where k is n-1.
 
 	// Counting backwards along the spiral,
 	// starting from the bottom-right cell,
 	// the distance to our input cell is d.
 	d := n*n - spiral
-	if d >= 4*n {
+	if d > 4*k {
 		log.Fatal("oh no")
 	}
 
 	// side is 0 (bottom) to 3 (right), as per diagram.
-	side := d / n
+	side := d / k
 	// Convert d to a distance from the corner
 	// that is at least as large as spiral.
-	d = d % n
+	d = d % k
 
 	// Now we are nearly done.
 	// n and d are the coordinates of our input cell,
 	// but shifted from what we need to compute distance to
 	// cell 1.
-
-	fmt.Println(n, d)
 
 	p := n / 2
 	q := d - p
