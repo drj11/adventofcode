eedb6b7d82e660cacae614e0b3f9c6f6a318f023
David Jones <drj@pobox.com>
2017-12-03 09:14:37 +0000

    Compute size of grid and distance along edge

--- 3/3.go
+++ 3/3.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
 )
 
 func main() {
 	input := 312051
 
+	// Imagine the grid of memory cells is expanded
+	// until it is a square of cells just big enough
+	// to include the input cell.
+
 	// The input lies on on the outside edge
-	// of square grid of size n,
-	// where n is odd, and the bottom-right
-	// cell is n*n.
+	// of this square grid, which we will say is of size n.
+	// n is odd, and the bottom-right cell is n*n.
 	// Find n
 	n := int(math.Ceil(math.Sqrt(float64(input))))
 	n |= 1
 
-	fmt.Println(n)
+	// The 4 corners of the square grid are
+	// n*n, n*n - n, n*n - 2*n, n*n - 3*n
+
+	// Counting backwards along the spiral,
+	// starting from the bottom-right cell,
+	// the distance to out input cell is d.
+	d := n*n - input
+	if d >= 4*n {
+		log.Fatal("oh no")
+	}
+
+	// Convert d to a distance from its nearest corner.
+	d = d % n
+
+	// Now we are nearly done.
+
+	fmt.Println(n, d)
 }
