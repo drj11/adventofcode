2bb147e0b9715f013192bf6a63c0d9446c09820f
David Jones <drj@pobox.com>
2017-12-06 19:34:52 +0000

    First version

---
+++ 5/5.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	inp, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var m []int
+
+	lines := bufio.NewScanner(inp)
+	for lines.Scan() {
+		line := lines.Text()
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		m = append(m, i)
+	}
+	fmt.Println(len(m))
+
+	cycles := escape(m)
+	fmt.Println(cycles)
+}
+
+func escape(m []int) int {
+	clock := 0
+	i := 0
+
+	for {
+		clock++
+		n := i + m[i]
+		m[i]++
+		i = n
+		if i >= len(m) {
+			return clock
+		}
+	}
+}
