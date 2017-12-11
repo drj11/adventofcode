0539adb620e3f4aee17097d1e2dc2d8577cfc6f5
David Jones <drj@pobox.com>
2017-12-11 21:32:06 +0000

    Print unique not-child node

--- 8/8.go
+++ 8/8.go
@@ -17,15 +17,31 @@ func main() {
 	re := regexp.MustCompile(
 		"(\\w+)\\s*\\((\\d+)\\)(?:\\s*->\\s*(.*))?")
 
-        cre := regexp.MustCompile("\\w+")
+	cre := regexp.MustCompile("\\w+")
+
+	ps := make(map[string]bool)
+	cs := make(map[string]bool)
 
 	lines := bufio.NewScanner(inp)
 	for lines.Scan() {
 		line := lines.Text()
 		match := re.FindStringSubmatch(line)
-                parent := match[1]
-                weight := match[2]
-                children := cre.FindAllString(match[3], -1)
-                fmt.Println(parent, weight, children)
+		parent := match[1]
+		weight := match[2]
+		children := cre.FindAllString(match[3], -1)
+		fmt.Println(parent, weight, children)
+
+		ps[parent] = true
+		for i := 0; i < len(children); i++ {
+			child := children[i]
+			cs[child] = true
+		}
+
+	}
+
+	for p, _ := range ps {
+		if !cs[p] {
+			fmt.Println(p)
+		}
 	}
 }
