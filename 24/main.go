9ebe7509bde62300fe154c2af28da34d4175d13e
David Jones <drj@pobox.com>
2017-12-31 14:10:50 +0000

    Find all bridges

--- 24/main.go
+++ 24/main.go
@@ -41,4 +41,49 @@ func main() {
 		components = append(components, component)
 	}
 	fmt.Println(components)
+	bridge(0, components)
+}
+
+var m int
+
+// cs are all the components
+// i is the number of components that
+// have been made into a bridge (the first i of cs).
+func bridge(i int, cs []Component) {
+	port := 0
+	if i > 0 {
+		port = cs[i-1].b
+	}
+
+	// extend the bridge by scanning forwards to find
+	// a component matching the port.
+	// When found, swap it with the component at index i.
+	found := false
+	for j := i; j < len(cs); j++ {
+		c := cs[j]
+		if c.b == port {
+			c.a, c.b = c.b, c.a
+		}
+		if c.a == port {
+			cs[i], cs[j] = c, cs[i]
+			bridge(i+1, cs)
+			found = true
+		}
+	}
+	if !found {
+		s := strength(cs[:i])
+		if s > m {
+			m = s
+		}
+		fmt.Println(m, s)
+	}
+}
+
+func strength(cs []Component) int {
+	a := 0
+	for _, c := range cs {
+		a += c.a
+		a += c.b
+	}
+	return a
 }
